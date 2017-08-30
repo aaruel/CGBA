428e3acfb834fa39d73d7b5eed885122974fa47a
aaruel <wmakit88@gmail.com>
2017-08-29 23:37:32 -0400

    5x instructions

--- cpu/interpreter.c
+++ cpu/interpreter.c
@@ -675,6 +675,86 @@ void _ip_LD_C_A() {
     _ip_LD_r_r(REG_C, REG_A);
 }
 
+// 0x50
+void _ip_LD_D_B() {
+    _ip_LD_r_r(REG_D, REG_B);
+}
+
+// 0x51
+void _ip_LD_D_C() {
+    _ip_LD_r_r(REG_D, REG_C);
+}
+
+// 0x52
+void _ip_LD_D_D() {
+    _ip_LD_r_r(REG_D, REG_D);
+}
+
+// 0x53
+void _ip_LD_D_E() {
+    _ip_LD_r_r(REG_D, REG_E);
+}
+
+// 0x54
+void _ip_LD_D_H() {
+    _ip_LD_r_r(REG_D, REG_H);
+}
+
+// 0x55
+void _ip_LD_D_L() {
+    _ip_LD_r_r(REG_D, REG_L);
+}
+
+// 0x56
+void _ip_LD_D_dHL() {
+    _ip_LD_r_drr(REG_D, REG_H, REG_L);
+}
+
+// 0x57
+void _ip_LD_D_A() {
+    _ip_LD_r_r(REG_D, REG_A);
+}
+
+// 0x58
+void _ip_LD_E_B() {
+    _ip_LD_r_r(REG_E, REG_B);
+}
+
+// 0x59
+void _ip_LD_E_C() {
+    _ip_LD_r_r(REG_E, REG_C);
+}
+
+// 0x5A
+void _ip_LD_E_D() {
+    _ip_LD_r_r(REG_E, REG_D);
+}
+
+// 0x5B
+void _ip_LD_E_E() {
+    _ip_LD_r_r(REG_E, REG_E);
+}
+
+// 0x5C
+void _ip_LD_E_H() {
+    _ip_LD_r_r(REG_E, REG_H);
+}
+
+// 0x5D
+void _ip_LD_E_L() {
+    _ip_LD_r_r(REG_E, REG_L);
+}
+
+// 0x5E
+void _ip_LD_E_dHL() {
+    _ip_LD_r_drr(REG_E, REG_H, REG_L);
+}
+
+// 0x5F
+void _ip_LD_E_A() {
+    _ip_LD_r_r(REG_E, REG_A);
+}
+
 
 // opcode map
 static void (*_ip_opcodes[])() = {
@@ -683,6 +763,7 @@ static void (*_ip_opcodes[])() = {
     /* 2x */ _ip_JR_NZ_r8, _ip_LD_HL_d16, _ip_LD_dHLp_A, _ip_INC_HL, _ip_INC_H, _ip_DEC_H, _ip_LD_H_d8, _ip_DAA, _ip_JR_Z_r8, _ip_ADD_HL_HL, _ip_LD_A_dHL, _ip_DEC_HL, _ip_INC_L, _ip_DEC_L, _ip_LD_L_d8, _ip_CPL,
     /* 3x */ _ip_JR_NC_r8, _ip_LD_SP_d16, _ip_LD_dHLd_A, _ip_INC_SP, _ip_INC_dHL, _ip_DEC_dHL, _ip_LD_dHL_d8, _ip_SCF, _ip_JR_C_r8, _ip_ADD_HL_SP, _ip_LD_A_dHLd, _ip_DEC_SP, _ip_INC_A, _ip_DEC_A, _ip_LD_A_d8, _ip_CCF,
     /* 4x */ _ip_LD_B_B, _ip_LD_B_C, _ip_LD_B_D, _ip_LD_B_E, _ip_LD_B_H, _ip_LD_B_L, _ip_LD_B_dHL, _ip_LD_B_A, _ip_LD_C_B, _ip_LD_C_C, _ip_LD_C_D, _ip_LD_C_E, _ip_LD_C_H, _ip_LD_C_L, _ip_LD_C_dHL, _ip_LD_C_A,
+    /* 5x */ _ip_LD_D_B, _ip_LD_D_C, _ip_LD_D_D, _ip_LD_D_E, _ip_LD_D_H, _ip_LD_D_L, _ip_LD_D_dHL, _ip_LD_D_A, _ip_LD_E_B, _ip_LD_E_C, _ip_LD_E_D, _ip_LD_E_E, _ip_LD_E_H, _ip_LD_E_L, _ip_LD_E_dHL, _ip_LD_E_A,
 };
 
 ///////**** Public ****///////

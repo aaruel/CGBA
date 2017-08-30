4f532a13c6f6a93bbb0a5749e02c22bb8ef23535
aaruel <wmakit88@gmail.com>
2017-08-30 00:03:29 -0400

    7x instructions

--- cpu/interpreter.c
+++ cpu/interpreter.c
@@ -480,7 +480,7 @@ void _ip_ADD_HL_HL() {
 }
 
 // 0x2A
-void _ip_LD_A_dHL() {
+void _ip_LD_A_dHLp() {
     _ip_LD_r_drr(REG_A, REG_H, REG_L);
 }
 
@@ -757,82 +757,165 @@ void _ip_LD_E_A() {
 
 // 0x60
 void _ip_LD_H_B() {
-    _ip_LD_r_r(REG_D, REG_B);
+    _ip_LD_r_r(REG_H, REG_B);
 }
 
 // 0x61
 void _ip_LD_H_C() {
-    _ip_LD_r_r(REG_D, REG_C);
+    _ip_LD_r_r(REG_H, REG_C);
 }
 
 // 0x62
 void _ip_LD_H_D() {
-    _ip_LD_r_r(REG_D, REG_D);
+    _ip_LD_r_r(REG_H, REG_D);
 }
 
 // 0x63
 void _ip_LD_H_E() {
-    _ip_LD_r_r(REG_D, REG_E);
+    _ip_LD_r_r(REG_H, REG_E);
 }
 
 // 0x64
 void _ip_LD_H_H() {
-    _ip_LD_r_r(REG_D, REG_H);
+    _ip_LD_r_r(REG_H, REG_H);
 }
 
 // 0x65
 void _ip_LD_H_L() {
-    _ip_LD_r_r(REG_D, REG_L);
+    _ip_LD_r_r(REG_H, REG_L);
 }
 
 // 0x66
 void _ip_LD_H_dHL() {
-    _ip_LD_r_drr(REG_D, REG_H, REG_L);
+    _ip_LD_r_drr(REG_H, REG_H, REG_L);
 }
 
 // 0x67
 void _ip_LD_H_A() {
-    _ip_LD_r_r(REG_D, REG_A);
+    _ip_LD_r_r(REG_H, REG_A);
 }
 
 // 0x68
 void _ip_LD_L_B() {
-    _ip_LD_r_r(REG_E, REG_B);
+    _ip_LD_r_r(REG_L, REG_B);
 }
 
 // 0x69
 void _ip_LD_L_C() {
-    _ip_LD_r_r(REG_E, REG_C);
+    _ip_LD_r_r(REG_L, REG_C);
 }
 
 // 0x6A
 void _ip_LD_L_D() {
-    _ip_LD_r_r(REG_E, REG_D);
+    _ip_LD_r_r(REG_L, REG_D);
 }
 
 // 0x6B
 void _ip_LD_L_E() {
-    _ip_LD_r_r(REG_E, REG_E);
+    _ip_LD_r_r(REG_L, REG_E);
 }
 
 // 0x6C
 void _ip_LD_L_H() {
-    _ip_LD_r_r(REG_E, REG_H);
+    _ip_LD_r_r(REG_L, REG_H);
 }
 
 // 0x6D
 void _ip_LD_L_L() {
-    _ip_LD_r_r(REG_E, REG_L);
+    _ip_LD_r_r(REG_L, REG_L);
 }
 
 // 0x6E
 void _ip_LD_L_dHL() {
-    _ip_LD_r_drr(REG_E, REG_H, REG_L);
+    _ip_LD_r_drr(REG_L, REG_H, REG_L);
 }
 
 // 0x6F
 void _ip_LD_L_A() {
-    _ip_LD_r_r(REG_E, REG_A);
+    _ip_LD_r_r(REG_L, REG_A);
+}
+
+// 0x70
+void _ip_LD_dHL_B() {
+    _ip_LD_drr_r(REG_H, REG_L, REG_B);
+}
+
+// 0x71
+void _ip_LD_dHL_C() {
+    _ip_LD_drr_r(REG_H, REG_L, REG_C);
+}
+
+// 0x72
+void _ip_LD_dHL_D() {
+    _ip_LD_drr_r(REG_H, REG_L, REG_D);
+}
+
+// 0x73
+void _ip_LD_dHL_E() {
+    _ip_LD_drr_r(REG_H, REG_L, REG_E);
+}
+
+// 0x74
+void _ip_LD_dHL_H() {
+    _ip_LD_drr_r(REG_H, REG_L, REG_H);
+}
+
+// 0x75
+void _ip_LD_dHL_L() {
+    _ip_LD_drr_r(REG_H, REG_L, REG_L);
+}
+
+// 0x76
+void _ip_HALT() {
+    // HALT!
+    sm_set_reg_stop(1);
+    sm_inc_clock(1);
+    getchar();
+}
+
+// 0x77
+void _ip_LD_dHL_A() {
+    _ip_LD_drr_r(REG_H, REG_L, REG_A);
+}
+
+// 0x78
+void _ip_LD_A_B() {
+    _ip_LD_r_r(REG_A, REG_B);
+}
+
+// 0x79
+void _ip_LD_A_C() {
+    _ip_LD_r_r(REG_A, REG_C);
+}
+
+// 0x7A
+void _ip_LD_A_D() {
+    _ip_LD_r_r(REG_A, REG_D);
+}
+
+// 0x7B
+void _ip_LD_A_E() {
+    _ip_LD_r_r(REG_A, REG_E);
+}
+
+// 0x7C
+void _ip_LD_A_H() {
+    _ip_LD_r_r(REG_A, REG_H);
+}
+
+// 0x7D
+void _ip_LD_A_L() {
+    _ip_LD_r_r(REG_A, REG_L);
+}
+
+// 0x7E
+void _ip_LD_A_dHL() {
+    _ip_LD_r_drr(REG_A, REG_H, REG_L);
+}
+
+// 0x7F
+void _ip_LD_A_A() {
+    _ip_LD_r_r(REG_A, REG_A);
 }
 
 
@@ -840,11 +923,12 @@ void _ip_LD_L_A() {
 static void (*_ip_opcodes[])() = {
     /* 0x */ _ip_NOP, _ip_LD_BC_d16, _ip_LD_dBC_A, _ip_INC_BC, _ip_INC_B, _ip_DEC_B, _ip_LD_B_d8, _ip_RLC_A, _ip_LD_da16_SP, _ip_ADD_HL_BC, _ip_LD_A_dBC, _ip_DEC_BC, _ip_INC_C, _ip_DEC_C, _ip_LD_C_d8, _ip_RRC_A,
     /* 1x */ _ip_STOP, _ip_LD_DE_d16, _ip_LD_dDE_A, _ip_INC_DE, _ip_INC_D, _ip_DEC_D, _ip_LD_D_d8, _ip_RLA, _ip_JR_r8, _ip_ADD_HL_DE, _ip_LD_A_dDE, _ip_DEC_DE, _ip_INC_E, _ip_DEC_E, _ip_LD_E_d8, _ip_RRA,
-    /* 2x */ _ip_JR_NZ_r8, _ip_LD_HL_d16, _ip_LD_dHLp_A, _ip_INC_HL, _ip_INC_H, _ip_DEC_H, _ip_LD_H_d8, _ip_DAA, _ip_JR_Z_r8, _ip_ADD_HL_HL, _ip_LD_A_dHL, _ip_DEC_HL, _ip_INC_L, _ip_DEC_L, _ip_LD_L_d8, _ip_CPL,
+    /* 2x */ _ip_JR_NZ_r8, _ip_LD_HL_d16, _ip_LD_dHLp_A, _ip_INC_HL, _ip_INC_H, _ip_DEC_H, _ip_LD_H_d8, _ip_DAA, _ip_JR_Z_r8, _ip_ADD_HL_HL, _ip_LD_A_dHLp, _ip_DEC_HL, _ip_INC_L, _ip_DEC_L, _ip_LD_L_d8, _ip_CPL,
     /* 3x */ _ip_JR_NC_r8, _ip_LD_SP_d16, _ip_LD_dHLd_A, _ip_INC_SP, _ip_INC_dHL, _ip_DEC_dHL, _ip_LD_dHL_d8, _ip_SCF, _ip_JR_C_r8, _ip_ADD_HL_SP, _ip_LD_A_dHLd, _ip_DEC_SP, _ip_INC_A, _ip_DEC_A, _ip_LD_A_d8, _ip_CCF,
     /* 4x */ _ip_LD_B_B, _ip_LD_B_C, _ip_LD_B_D, _ip_LD_B_E, _ip_LD_B_H, _ip_LD_B_L, _ip_LD_B_dHL, _ip_LD_B_A, _ip_LD_C_B, _ip_LD_C_C, _ip_LD_C_D, _ip_LD_C_E, _ip_LD_C_H, _ip_LD_C_L, _ip_LD_C_dHL, _ip_LD_C_A,
     /* 5x */ _ip_LD_D_B, _ip_LD_D_C, _ip_LD_D_D, _ip_LD_D_E, _ip_LD_D_H, _ip_LD_D_L, _ip_LD_D_dHL, _ip_LD_D_A, _ip_LD_E_B, _ip_LD_E_C, _ip_LD_E_D, _ip_LD_E_E, _ip_LD_E_H, _ip_LD_E_L, _ip_LD_E_dHL, _ip_LD_E_A,
     /* 6x */ _ip_LD_H_B, _ip_LD_H_C, _ip_LD_H_D, _ip_LD_H_E, _ip_LD_H_H, _ip_LD_H_L, _ip_LD_H_dHL, _ip_LD_H_A, _ip_LD_L_B, _ip_LD_L_C, _ip_LD_L_D, _ip_LD_L_E, _ip_LD_L_H, _ip_LD_L_L, _ip_LD_L_dHL, _ip_LD_L_A,
+    /* 7x */ _ip_LD_dHL_B, _ip_LD_dHL_C, _ip_LD_dHL_D, _ip_LD_dHL_E, _ip_LD_dHL_H, _ip_LD_dHL_L, _ip_HALT, _ip_LD_dHL_A, _ip_LD_A_B, _ip_LD_A_C, _ip_LD_A_D, _ip_LD_A_E, _ip_LD_A_H, _ip_LD_A_L, _ip_LD_A_dHL, _ip_LD_A_A,
 };
 
 ///////**** Public ****///////
